ce91af267abef3619120bf549dad25c07a38236b
François <francois.gauti3r@gmail.com>
2023-08-02 14:33:54 +0200

    [EDELIVERY-11879] Implement solution to remove the jms asynchronicity from tests

--- Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/core/alert/AlertEventsTestIT.java
+++ Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/core/alert/AlertEventsTestIT.java
@@ -17,23 +17,20 @@ import eu.domibus.core.alerts.service.EventServiceImpl;
 import eu.domibus.core.user.ui.UserDao;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Ion Perpegel
  * @since 5.0
  */
-public class AlertEventsTestIT extends AbstractIT {
+@SuppressWarnings("rawtypes")
+class AlertEventsTestIT extends AbstractIT {
 
     @Autowired
     private EventServiceImpl eventService;
@@ -63,122 +60,117 @@ public class AlertEventsTestIT extends AbstractIT {
     }
 
     @Test
-    public void sendEventStartDateStopped() throws InterruptedException {
+    void sendEventStartDateStopped() {
         EventType eventType = EventType.ARCHIVING_START_DATE_STOPPED;
         eventService.enqueueEvent(eventType, eventType.name(), new EventProperties());
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+        Assertions.assertEquals(1, dispatchedAlerts.size());
         Alert alert = dispatchedAlerts.get(0);
-        Assertions.assertEquals(alert.getAlertType(), AlertType.ARCHIVING_START_DATE_STOPPED);
-        Assertions.assertEquals(alert.getEvents().size(), 1);
+        Assertions.assertEquals(AlertType.ARCHIVING_START_DATE_STOPPED, alert.getAlertType());
+        Assertions.assertEquals(1, alert.getEvents().size());
         Assertions.assertEquals(alert.getEvents().toArray(new Event[0])[0].getType(), eventType);
     }
 
     @Test
-    @Disabled("EDELIVERY-6896")
-    public void sendEventMessageNotFinal() throws InterruptedException {
+    void sendEventMessageNotFinal() {
         String messageId = "messageId";
         MessageStatus messageStatus = MessageStatus.RECEIVED;
 
         eventService.enqueueEvent(EventType.ARCHIVING_MESSAGES_NON_FINAL, messageId, new EventProperties(messageId, messageStatus.name()));
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+        Assertions.assertEquals(1, dispatchedAlerts.size());
         Alert alert = dispatchedAlerts.get(0);
-        Assertions.assertEquals(alert.getAlertType(), AlertType.ARCHIVING_MESSAGES_NON_FINAL);
-        Assertions.assertEquals(alert.getEvents().size(), 1);
+        Assertions.assertEquals(AlertType.ARCHIVING_MESSAGES_NON_FINAL, alert.getAlertType());
+        Assertions.assertEquals(1, alert.getEvents().size());
         Event event = alert.getEvents().toArray(new Event[0])[0];
-        Assertions.assertEquals(event.getType(), EventType.ARCHIVING_MESSAGES_NON_FINAL);
+        Assertions.assertEquals(EventType.ARCHIVING_MESSAGES_NON_FINAL, event.getType());
         Map<String, AbstractPropertyValue> properties = event.getProperties();
-        Assertions.assertEquals(properties.size(), 3);
+        Assertions.assertEquals(3, properties.size());
         Assertions.assertEquals(properties.get("OLD_STATUS").getValue(), messageStatus.name());
         Assertions.assertEquals(properties.get("MESSAGE_ID").getValue(), messageId);
         Assertions.assertEquals(properties.get("EVENT_IDENTIFIER").getValue(), messageId);
     }
 
     @Test
-    public void sendMessagingEvent() throws InterruptedException {
+    void sendMessagingEvent() {
         String messageId = this.getClass().getName() + "msg-test-1";
         UserMessageLog uml = messageDaoTestUtil.createTestMessage(messageId);
         eventService.enqueueMessageStatusChangedEvent(messageId, MessageStatus.SEND_ENQUEUED, MessageStatus.SEND_FAILURE, MSHRole.SENDING);
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+        Assertions.assertEquals(1, dispatchedAlerts.size());
         Alert alert = dispatchedAlerts.get(0);
-        Assertions.assertEquals(alert.getAlertType(), AlertType.MSG_STATUS_CHANGED);
-        Assertions.assertEquals(alert.getEvents().size(), 1);
+        Assertions.assertEquals(AlertType.MSG_STATUS_CHANGED, alert.getAlertType());
+        Assertions.assertEquals(1, alert.getEvents().size());
         Event event = alert.getEvents().toArray(new Event[0])[0];
-        Assertions.assertEquals(event.getType(), EventType.MSG_STATUS_CHANGED);
+        Assertions.assertEquals(EventType.MSG_STATUS_CHANGED, event.getType());
         Map<String, AbstractPropertyValue> properties = event.getProperties();
-        Assertions.assertEquals(properties.size(), 4);
+        Assertions.assertEquals(4, properties.size());
         Assertions.assertEquals(properties.get("OLD_STATUS").getValue(), MessageStatus.SEND_ENQUEUED.name());
         Assertions.assertEquals(properties.get("NEW_STATUS").getValue(), MessageStatus.SEND_FAILURE.name());
         Assertions.assertEquals(properties.get("MESSAGE_ID").getValue(), messageId);
 
-        messageDaoTestUtil.deleteMessages(Arrays.asList(uml.getEntityId()));
+        messageDaoTestUtil.deleteMessages(Collections.singletonList(uml.getEntityId()));
     }
 
     @Test
-    public void sendMessagingEventStatusWithoutAlert() throws InterruptedException {
+    void sendMessagingEventStatusWithoutAlert() {
         String messageId = this.getClass().getName() + "msg-test-2";
         UserMessageLog uml = messageDaoTestUtil.createTestMessage(messageId);
         eventService.enqueueMessageStatusChangedEvent(messageId, MessageStatus.SEND_ENQUEUED, MessageStatus.ACKNOWLEDGED, MSHRole.SENDING);
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 0);
 
-        messageDaoTestUtil.deleteMessages(Arrays.asList(uml.getEntityId()));
+        Assertions.assertEquals(0, dispatchedAlerts.size());
+
+        messageDaoTestUtil.deleteMessages(Collections.singletonList(uml.getEntityId()));
     }
 
     @Test
-    public void sendRepetitiveAlert() throws InterruptedException {
+    void sendRepetitiveAlert() {
         EventType eventType = EventType.PLUGIN_PASSWORD_EXPIRED;
         UserEntityBase user = userDao.listUsers().get(0);
         int maxPasswordAgeInDays = 10;
 
         eventService.enqueuePasswordExpirationEvent(eventType, user, maxPasswordAgeInDays);
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+
+        Assertions.assertEquals(1, dispatchedAlerts.size());
         Alert alert = dispatchedAlerts.get(0);
-        Assertions.assertEquals(alert.getAlertType(), AlertType.PLUGIN_PASSWORD_EXPIRED);
-        Assertions.assertEquals(alert.getEvents().size(), 1);
+        Assertions.assertEquals(AlertType.PLUGIN_PASSWORD_EXPIRED, alert.getAlertType());
+        Assertions.assertEquals(1, alert.getEvents().size());
         Assertions.assertEquals(alert.getEvents().toArray(new Event[0])[0].getType(), eventType);
 
         eventService.enqueuePasswordExpirationEvent(eventType, user, maxPasswordAgeInDays);
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+
+        Assertions.assertEquals(1, dispatchedAlerts.size());
 
         eventService.enqueuePasswordExpirationEvent(eventType, user, maxPasswordAgeInDays);
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+
+        Assertions.assertEquals(1, dispatchedAlerts.size());
     }
 
     @Test
-    public void sendFrequencyAlert() throws InterruptedException {
+    void sendFrequencyAlert() {
         String partitionName = "partitionName";
 
         eventService.enqueueEvent(EventType.PARTITION_CHECK, partitionName, new EventProperties(partitionName));
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+        Assertions.assertEquals(1, dispatchedAlerts.size());
         Alert alert = dispatchedAlerts.get(0);
-        Assertions.assertEquals(alert.getAlertType(), AlertType.PARTITION_CHECK);
-        Assertions.assertEquals(alert.getEvents().size(), 1);
-        Assertions.assertEquals(alert.getEvents().toArray(new Event[0])[0].getType(), EventType.PARTITION_CHECK);
+        Assertions.assertEquals(AlertType.PARTITION_CHECK, alert.getAlertType());
+        Assertions.assertEquals(1, alert.getEvents().size());
+        Assertions.assertEquals(EventType.PARTITION_CHECK, alert.getEvents().toArray(new Event[0])[0].getType());
 
         eventService.enqueueEvent(EventType.PARTITION_CHECK, partitionName, new EventProperties(partitionName));
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+
+        Assertions.assertEquals(1, dispatchedAlerts.size());
 
         eventService.enqueueEvent(EventType.PARTITION_CHECK, partitionName, new EventProperties(partitionName));
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+
+        Assertions.assertEquals(1, dispatchedAlerts.size());
     }
 
     @Test
-    public void sendConnMonitorAlert() throws InterruptedException {
+    void sendConnMonitorAlert() {
         MessageStatus oldStatus = MessageStatus.SEND_ENQUEUED;
         MessageStatus newStatus = MessageStatus.SEND_FAILURE;
         String fromParty = "fromParty";
@@ -187,16 +179,15 @@ public class AlertEventsTestIT extends AbstractIT {
 
         eventService.enqueueMonitoringEvent(messageId, MSHRole.RECEIVING, oldStatus, newStatus, fromParty, toParty);
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 1);
+        Assertions.assertEquals(1, dispatchedAlerts.size());
         Alert alert = dispatchedAlerts.get(0);
-        Assertions.assertEquals(alert.getAlertType(), AlertType.CONNECTION_MONITORING_FAILED);
-        Assertions.assertEquals(alert.getEvents().size(), 1);
-        Assertions.assertEquals(alert.getEvents().toArray(new Event[0])[0].getType(), EventType.CONNECTION_MONITORING_FAILED);
+        Assertions.assertEquals(AlertType.CONNECTION_MONITORING_FAILED, alert.getAlertType());
+        Assertions.assertEquals(1, alert.getEvents().size());
+        Assertions.assertEquals(EventType.CONNECTION_MONITORING_FAILED, alert.getEvents().toArray(new Event[0])[0].getType());
     }
 
     @Test
-    public void sendConnMonitorAlertNoAlert() throws InterruptedException {
+    void sendConnMonitorAlertNoAlert() {
         MessageStatus oldStatus = MessageStatus.SEND_ENQUEUED;
         MessageStatus newStatus = MessageStatus.ACKNOWLEDGED;
         String fromParty = "fromParty";
@@ -205,8 +196,7 @@ public class AlertEventsTestIT extends AbstractIT {
 
         eventService.enqueueMonitoringEvent(messageId, MSHRole.SENDING, oldStatus, newStatus, fromParty, toParty);
 
-        Thread.sleep(1000);
-        Assertions.assertEquals(dispatchedAlerts.size(), 0);
+        Assertions.assertEquals(0, dispatchedAlerts.size());
     }
 
 }
