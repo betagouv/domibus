9907b21a0474a3d48a43b3cf2420144407208e8d
soumya <Soumya.AZHIKODE-CHANDRAN@ext.ec.europa.eu>
2023-08-03 17:52:48 +0200

    [EDELIVERY-3891] -Merge conflict fix

--- Core/Domibus-MSH/src/main/java/eu/domibus/core/property/listeners/DomainTitleChangeListener.java
+++ Core/Domibus-MSH/src/main/java/eu/domibus/core/property/listeners/DomainTitleChangeListener.java
@@ -1,9 +1,11 @@
 package eu.domibus.core.property.listeners;
 
 import eu.domibus.api.multitenancy.DomainService;
+import eu.domibus.api.property.DomibusConfigurationService;
 import eu.domibus.api.property.DomibusPropertyChangeListener;
+import eu.domibus.api.property.DomibusPropertyException;
+import eu.domibus.core.multitenancy.DomibusDomainException;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMAIN_TITLE;
@@ -16,9 +18,16 @@ import static eu.domibus.api.property.DomibusPropertyMetadataManagerSPI.DOMAIN_T
  */
 @Service
 public class DomainTitleChangeListener implements DomibusPropertyChangeListener {
+    public static int DOMAIN_TITLE_MAX_LENGTH = 50;
 
-    @Autowired
-    private DomainService domainService;
+    private final DomainService domainService;
+
+    private final DomibusConfigurationService domibusConfigurationService;
+
+    public DomainTitleChangeListener(DomainService domainService, DomibusConfigurationService domibusConfigurationService) {
+        this.domainService = domainService;
+        this.domibusConfigurationService = domibusConfigurationService;
+    }
 
     @Override
     public boolean handlesProperty(String propertyName) {
@@ -28,12 +37,11 @@ public class DomainTitleChangeListener implements DomibusPropertyChangeListener 
     @Override
     public void propertyValueChanged(String domainCode, String propertyName, String propertyValue) {
 
-        if ((StringUtils.length(propertyValue) > DOMAIN_TITLE_MAX_LENGTH)) {
+        if (StringUtils.length(propertyValue) > DOMAIN_TITLE_MAX_LENGTH) {
             throw new DomibusPropertyException(String.format("Cannot change domain title to [%s] because it is greater than the maximum allowed length [%s].", propertyValue, DOMAIN_TITLE_MAX_LENGTH));
         }
 
-
-        if(domibusConfigurationService.isSingleTenantAware()){
+        if (domibusConfigurationService.isSingleTenantAware()) {
             throw new DomibusDomainException("Cannot change domain title in single tenancy configuration.");
         }
         this.domainService.refreshDomain(domainCode);
