d9e199d5ef8ff673ccaf4e3c8f5804583f3f5c32
Ionut Breaz <ionut.breaz@gmail.com>
2023-08-08 14:33:02 +0300

    [EDELIVERY-11884] add tests for MessageLogResource

---
+++ Core/Domibus-MSH-test-common/src/main/java/eu/domibus/test/common/CsvUtil.java
@@ -0,0 +1,26 @@
+package eu.domibus.test.common;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class CsvUtil {
+    public List<List<String>> getCsvRecords(String csv) throws IOException, CsvValidationException {
+        List<List<String>> records = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new StringReader(csv))) {
+            String[] values = csvReader.readNext();
+            while (values != null) {
+                records.add(Arrays.asList(values));
+                values = csvReader.readNext();
+            }
+        }
+        return records;
+    }
+}
--- Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/web/rest/MessageLogResourceIT.java
+++ Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/web/rest/MessageLogResourceIT.java
@@ -1,90 +1,169 @@
 package eu.domibus.web.rest;
 
 import eu.domibus.AbstractIT;
-import eu.domibus.api.model.UserMessage;
-import eu.domibus.core.message.UserMessageDao;
-import eu.domibus.web.rest.ro.MessageLogResultRO;
-import org.junit.jupiter.api.*;
+import eu.domibus.api.plugin.BackendConnectorService;
+import eu.domibus.core.ebms3.receiver.MSHWebservice;
+import eu.domibus.core.message.MessageStatusDao;
+import eu.domibus.core.message.dictionary.MshRoleDao;
+import eu.domibus.core.payload.persistence.filesystem.PayloadFileStorageProvider;
+import eu.domibus.core.plugin.BackendConnectorProvider;
+import eu.domibus.plugin.BackendConnector;
+import eu.domibus.test.common.CsvUtil;
+import eu.domibus.test.common.SoapSampleUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.transaction.Transactional;
+import javax.xml.soap.SOAPMessage;
 import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Transactional
 class MessageLogResourceIT extends AbstractIT {
     private MockMvc mockMvc;
+
+    @Configuration
+    static class ContextConfiguration {
+        @Primary
+        @Bean
+        public BackendConnectorService backendConnectorProvider() {
+            return Mockito.mock(BackendConnectorService.class);
+        }
+    }
 
     @Autowired
     private MessageLogResource messageLogResource;
 
     @Autowired
-    private UserMessageDao userMessageDao;
+    MessageStatusDao messageStatusDao;
+
+    @Autowired
+    MshRoleDao mshRoleDao;
+
+    @Autowired
+    BackendConnectorProvider backendConnectorProvider;
+
+    @Autowired
+    SoapSampleUtil soapSampleUtil;
+
+    @Autowired
+    CsvUtil csvUtil;
+
+    @Autowired
+    MSHWebservice mshWebservice;
+
+    @Autowired
+    protected PayloadFileStorageProvider payloadFileStorageProvider;
+
+    private final String messageId = "43bb6883-77d2-4a41-bac4-52a485d50084@domibus.eu";
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
         this.mockMvc = MockMvcBuilders.standaloneSetup(messageLogResource)
                 .build();
+
+        uploadPMode();
+        payloadFileStorageProvider.initialize();
+        BackendConnector backendConnector = Mockito.mock(BackendConnector.class);
+        Mockito.when(backendConnectorProvider.getBackendConnector(Mockito.any(String.class))).thenReturn(backendConnector);
+
+        String filename = "SOAPMessage2.xml";
+        SOAPMessage soapMessage = soapSampleUtil.createSOAPMessage(filename, messageId);
+        mshWebservice.invoke(soapMessage);
     }
 
-    @AfterEach
-    void tearDown() {
-    }
-
-    //TODO needs data
     @Test
-    @Disabled
-    void getMessageLog() throws Exception {
-        List<UserMessage> all = userMessageDao.findAll();
-
-        String messageId = "msg_ack_100";
-        MvcResult result = mockMvc.perform(get("/rest/messagelog")
+    void getMessageLog_OK() throws Exception {
+        mockMvc.perform(get("/rest/messagelog")
                         .with(httpBasic(TEST_PLUGIN_USERNAME, TEST_PLUGIN_PASSWORD))
                         .with(csrf())
                         .param("messageId", messageId)
                 )
                 .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.count").value(1))
+                .andExpect(jsonPath("$.messageLogEntries[0].messageId").value(messageId))
+                .andExpect(jsonPath("$.messageLogEntries[0].fromPartyId").value("domibus-blue"))
+                .andExpect(jsonPath("$.messageLogEntries[0].toPartyId").value("domibus-red"))
+                .andExpect(jsonPath("$.messageLogEntries[0].messageStatus").value("RECEIVED"))
+                .andExpect(jsonPath("$.messageLogEntries[0].notificationStatus").value("REQUIRED"))
+                .andExpect(jsonPath("$.messageLogEntries[0].mshRole").value("RECEIVING"))
+                .andExpect(jsonPath("$.messageLogEntries[0].messageType").value("USER_MESSAGE"));
+    }
+
+    @Test
+    void getCsv_OK() throws Exception {
+        MvcResult result = mockMvc.perform(get("/rest/messagelog/csv")
+                        .contentType("text/html; charset=UTF-8")
+                        .with(httpBasic(TEST_PLUGIN_USERNAME, TEST_PLUGIN_PASSWORD))
+                        .with(csrf())
+                        .param("messageId", messageId)
+                        .param("orderBy", "received")
+                        .param("asc", "false")
+                        .param("page", "0")
+                        .param("pageSize", "10")
+
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(header().exists("Content-Disposition"))
                 .andReturn();
 
-        String content = result.getResponse().getContentAsString();
-        Assertions.assertNotNull(content);
-        MessageLogResultRO errorLogResultRO = objectMapper.readValue(content, MessageLogResultRO.class);
-        Assertions.assertNotNull(errorLogResultRO);
+        String csv = result.getResponse().getContentAsString();
+        Assertions.assertNotNull(csv);
+
+        List<List<String>> csvRecords = csvUtil.getCsvRecords(csv);
+        Assertions.assertEquals(2, csvRecords.size());
+        List<String> header = csvRecords.get(0);
+        List<String> row = csvRecords.get(1);
+        Assertions.assertEquals("Message Id", header.get(0));
+        Assertions.assertEquals(messageId, row.get(0));
+        Assertions.assertEquals("From Party Id", header.get(1));
+        Assertions.assertEquals("domibus-blue", row.get(1));
+        Assertions.assertEquals("To Party Id", header.get(2));
+        Assertions.assertEquals("domibus-red", row.get(2));
+        Assertions.assertEquals("Message Status", header.get(3));
+        Assertions.assertEquals("RECEIVED", row.get(3));
+        Assertions.assertEquals("AP Role", header.get(7));
+        Assertions.assertEquals("RECEIVING", row.get(7));
     }
 
-    //TODO IB !!!! write test for getCsv
     @Test
-    @Disabled
-    void getCsv() {
+    void getLastTestSent_Not_Found() throws Exception {
+        Exception exception = Assertions.assertThrows(Exception.class,
+                () -> mockMvc.perform(get("/rest/messagelog/test/outgoing/latest")
+                    .with(httpBasic(TEST_PLUGIN_USERNAME, TEST_PLUGIN_PASSWORD))
+                    .with(csrf())
+                    .param("partyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1")
+                    .param("senderPartyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4")
+                )
+        );
+        Assertions.assertNotNull(exception);
+        Assertions.assertTrue(exception.getCause().getMessage().contains("[DOM_001]:No User message found for party"));
     }
 
-    //TODO needs data
     @Test
-    @Disabled
-    void getLastTestSent_OK() throws Exception {
-        List<UserMessage> all = userMessageDao.findAll();
-
-        MvcResult result = mockMvc.perform(get("/rest/messagelog/test/outgoing/latest")
+    void getLastTestReceived_Not_Found() throws Exception {
+        Exception exception = Assertions.assertThrows(Exception.class,
+                () -> mockMvc.perform(get("/rest/messagelog/test/incoming/latest")
                         .with(httpBasic(TEST_PLUGIN_USERNAME, TEST_PLUGIN_PASSWORD))
                         .with(csrf())
                         .param("partyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1")
                         .param("senderPartyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4")
                 )
-                .andExpect(status().is2xxSuccessful())
-                .andReturn();
-        // then
-        String content = result.getResponse().getContentAsString();
-        Assertions.assertNotNull(content);
-    }
-
-    //TODO IB !!!! write test for getLastTestReceived
-    @Test
-    @Disabled
-    void getLastTestReceived() {
+        );
+        Assertions.assertNotNull(exception);
+        Assertions.assertEquals("No Signal Message found.", exception.getCause().getMessage());
     }
 }
