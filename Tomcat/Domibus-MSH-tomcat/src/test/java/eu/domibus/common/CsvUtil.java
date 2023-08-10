8ab0c8b684810d2c833ab91914fbf4d76eee625d
Ionut Breaz <ionut.breaz@gmail.com>
2023-08-10 16:47:36 +0300

    [EDELIVERY-11884] address PR comments

--- Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/common/CsvUtil.java
+++ Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/common/CsvUtil.java
@@ -10,11 +10,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @author Ionut Breaz
+ * @since 5.1
+ */
+
+
 @Component
 public class CsvUtil {
     public List<List<String>> getCsvRecords(String csv) throws IOException, CsvValidationException {
         List<List<String>> records = new ArrayList<>();
-        try (CSVReader csvReader = new CSVReader(new StringReader(csv))) {
+        try (StringReader stringReader = new StringReader(csv);
+             CSVReader csvReader = new CSVReader(stringReader)) {
             String[] values = csvReader.readNext();
             while (values != null) {
                 records.add(Arrays.asList(values));
