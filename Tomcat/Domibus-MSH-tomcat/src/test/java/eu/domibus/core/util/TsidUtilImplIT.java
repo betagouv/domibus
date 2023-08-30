90687ec3d889b3363da89cdd782eb034c75521f9
Cosmin Baciu <Cosmin Baciu>
2023-08-30 16:14:15 +0200

    EDELIVERY-11528 - Replace table generator with a more performant one - part 3

--- Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/core/util/TsidUtilImplIT.java
+++ Tomcat/Domibus-MSH-tomcat/src/test/java/eu/domibus/core/util/TsidUtilImplIT.java
@@ -22,6 +22,7 @@ import java.util.Date;
 public class TsidUtilImplIT extends AbstractIT {
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(TsidUtilImplIT.class);
+    public static final long TSID_DIFFERENCE_BETWEEN_2_DATES_WITH_ONE_HOUR_DIFFERENCE = 15099494400000L;
 
     @Autowired
     TsidUtil tsidUtil;
@@ -61,5 +62,29 @@ public class TsidUtilImplIT extends AbstractIT {
         final long dateFromTsid = tsidUtil.getDateFromTsid(tsid);
         final Date extractedDateFromTsid = new Date(dateFromTsid);
         Assertions.assertEquals(initialDate, extractedDateFromTsid);
+    }
+
+    @Test
+    public void testTsidValueBetweenTwoDates() {
+        final ZonedDateTime zonedDateTime1 = LocalDateTime.of(2023, 1, 1, 1, 0, 0).atZone(ZoneOffset.UTC);
+        final long tsid1 = tsidUtil.zonedTimeDateToTsid(zonedDateTime1);
+
+        final ZonedDateTime zonedDateTime2 = LocalDateTime.of(2023, 1, 1, 2, 0, 0).atZone(ZoneOffset.UTC);
+        final long tsid2 = tsidUtil.zonedTimeDateToTsid(zonedDateTime2);
+
+        LOG.info("ZonedDateTime1 [{}], TSID1  [{}], ZonedDateTime2 [{}], TSID2  [{}],", zonedDateTime1, tsid1, zonedDateTime2, tsid2);
+        //15099494400000 represents the difference between two dates with one hour difference eg Date1; Date2=Date1 + 1h
+        Assertions.assertEquals(TSID_DIFFERENCE_BETWEEN_2_DATES_WITH_ONE_HOUR_DIFFERENCE, tsid2 - tsid1);
+
+        final ZonedDateTime zonedDateTime3 = LocalDateTime.of(2023, 1, 1, 1, 0, 0).atZone(ZoneOffset.UTC);
+        final long tsid3 = tsidUtil.zonedTimeDateToTsid(zonedDateTime3);
+
+        final ZonedDateTime zonedDateTime4 = LocalDateTime.of(2023, 1, 2, 1, 0, 0).atZone(ZoneOffset.UTC);
+        final long tsid4 = tsidUtil.zonedTimeDateToTsid(zonedDateTime4);
+
+        LOG.info("ZonedDateTime1 [{}], TSID1  [{}], ZonedDateTime2 [{}], TSID2  [{}],", zonedDateTime3, tsid3, zonedDateTime4, tsid4);
+        //24 hours difference between date 3 and date 4
+        Assertions.assertEquals(24 * TSID_DIFFERENCE_BETWEEN_2_DATES_WITH_ONE_HOUR_DIFFERENCE, tsid4 - tsid3);
+
     }
 }
